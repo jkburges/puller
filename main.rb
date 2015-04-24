aff9ff7d32a04357c5def4d39f104bde62b4ca23
Jon Burgess <jkburges@gmail.com>
2015-04-24 16:44:12 +1000

    Include merger details for closed pulls

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ org = 'aodn'
 
 client.org_repos(org).each do |repo|
 
-  pulls = client.pulls(repo.full_name)
+  pulls = client.pulls(repo.full_name, :state => :closed)
 
   repo = Octokit::Repository.new(repo.full_name)
 
@@ -21,12 +21,14 @@ client.org_repos(org).each do |repo|
     updated_at = pull.updated_at
 
     author = pull.user.login
+    state = pull.state
+    merger = pull.merged_by ? pull.merged_by.login : nil
 
     review_comments = client.pull_comments(repo.slug, pull.number)
     issue_comments = client.issue_comments(repo.slug, pull.number)
     comments = issue_comments.concat(review_comments).uniq
     commentors = comments.map { |c| c.user.login }.uniq.sort
 
-    puts "#{updated_at}, #{project}, #{url}, #{comments.length}, #{commentors}, #{author}"
+    puts "#{updated_at}, #{project}, #{url}, #{comments.length}, #{commentors}, #{author}, #{state}, #{merger}"
   end
 end
