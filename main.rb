a1f921c9c02a9e0b8225f12044b31894b8dd28a6
Jon Burgess <jkburges@gmail.com>
2015-04-24 15:24:21 +1000

    Include issue comments

--- main.rb
+++ main.rb
@@ -21,8 +21,10 @@ client.org_repos(org).each do |repo|
     updated_at = pull.updated_at
 
     author = pull.user.login
-    comments = client.pull_comments(repo.slug, pull.number)
 
+    review_comments = client.pull_comments(repo.slug, pull.number)
+    issue_comments = client.issue_comments(repo.slug, pull.number)
+    comments = issue_comments.concat(review_comments).uniq
     commentors = comments.map { |c| c.user.login }.uniq.sort
 
     puts "#{updated_at}, #{project}, #{url}, #{comments.length}, #{commentors}, #{author}"
