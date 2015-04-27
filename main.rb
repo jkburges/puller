a02c076d8423fb653e04660ec8843c522b92a594
Jon Burgess <jkburges@gmail.com>
2015-04-27 14:50:21 +1000

    Refactoring, and correctly printing the merger.

--- main.rb
+++ main.rb
@@ -2,33 +2,37 @@
 
 require 'octokit'
 
-Octokit.auto_paginate = true
-
-client = Octokit::Client.new(:netrc => true)
+@client = Octokit::Client.new(:netrc => true)
 
 org = 'aodn'
 
-client.org_repos(org).each do |repo|
+def print_stats(pull)
+  pull = @client.pull(pull.base.repo.full_name, pull.number)
 
-  pulls = client.pulls(repo.full_name, :state => :closed)
+  project_name = pull.base.repo.full_name
+  url = pull.html_url
+  updated_at = pull.updated_at
+
+  author = pull.user.login
+  state = pull.state
+
+  merger = pull.merged_by? ? pull.merged_by.login : nil
+
+  review_comments = @client.pull_comments(project_name, pull.number)
+  issue_comments = @client.issue_comments(project_name, pull.number)
+  comments = issue_comments.concat(review_comments).uniq
+  commentors = comments.map { |c| c.user.login }.uniq.sort
+
+  puts [updated_at, project_name, url, author, state, merger, comments.length, "#{commentors}"].join ', '
+end
+
+@client.org_repos(org).each do |repo|
+
+  pulls = @client.pulls(repo.full_name, :state => :closed)
 
   repo = Octokit::Repository.new(repo.full_name)
 
   pulls.each do |pull|
-
-    project = repo.slug
-    url = pull.html_url
-    updated_at = pull.updated_at
-
-    author = pull.user.login
-    state = pull.state
-    merger = pull.merged_by ? pull.merged_by.login : nil
-
-    review_comments = client.pull_comments(repo.slug, pull.number)
-    issue_comments = client.issue_comments(repo.slug, pull.number)
-    comments = issue_comments.concat(review_comments).uniq
-    commentors = comments.map { |c| c.user.login }.uniq.sort
-
-    puts "#{updated_at}, #{project}, #{url}, #{comments.length}, #{commentors}, #{author}, #{state}, #{merger}"
+    print_stats pull
   end
 end
